8f1e4f71507e82850d337066d078d4c1933bc98f
Wilfer Salas <sony634@hotmail.com>
2022-02-24 22:29:09 -0500

    refactor product services

--- src/services/products.service.ts
+++ src/services/products.service.ts
@@ -25,69 +25,75 @@ class ProductsService {
   };
 
   async find (serachTerm: any) {
-    const response = await axios(`https://api.mercadolibre.com/sites/MLA/search?q=${serachTerm}`)
-      .then(res => res.data)
-      .catch(err => console.log(err))
+    try {
+      const response = await axios(`https://api.mercadolibre.com/sites/MLA/search?q=${serachTerm}`)
+        .then(res => res.data)
+        .catch(err => console.log(err))
 
-    const categories = response?.filters.find((filter: any) => filter.id === 'category')?.values[0]
-      .path_from_root.map((path: any) => path.name)
+      const categories = response?.filters.find((filter: any) => filter.id === 'category')?.values[0]
+        .path_from_root.map((path: any) => path.name)
 
-    const products = {
-      author: {
-        name: 'Wilfer',
-        lastname: 'Salas'
-      },
-      categories,
-      items: response.results.map((product: Product) => {
-        return {
-          id: product?.id,
-          title: product?.title,
-          price: {
-            currency: product?.currency_id,
-            amount: product?.price,
-            decimals: product?.price
-          },
-          picture: product?.thumbnail,
-          condition: product.condition,
-          free_shipping: product?.shipping?.free_shipping
-        }
-      }).slice(0, 4)
+      const products = {
+        author: {
+          name: 'Wilfer',
+          lastname: 'Salas'
+        },
+        categories,
+        items: response.results.map((product: Product) => {
+          return {
+            id: product?.id,
+            title: product?.title,
+            price: {
+              currency: product?.currency_id,
+              amount: product?.price,
+              decimals: product?.price
+            },
+            picture: product?.thumbnail,
+            condition: product.condition,
+            free_shipping: product?.shipping?.free_shipping
+          }
+        }).slice(0, 4)
+      }
+
+      return products
+    } catch (error: any) {
+      return error.status
     }
-
-    return products
   };
 
   async findOne (id: string) {
-    const response = await axios(`https://api.mercadolibre.com/items/${id}`)
-      .then(res => res.data)
-      .catch(err => console.log(err))
+    try {
+      const response = await axios(`https://api.mercadolibre.com/items/${id}`)
+        .then(res => res.data)
 
-    const description = await axios(`https://api.mercadolibre.com/items/${id}/description`)
-      .then(res => res.data)
-      .catch(err => console.log(err))
+      const description = await axios(`https://api.mercadolibre.com/items/${id}/description`)
+        .then(res => res.data)
 
-    const product = {
-      author: {
-        name: 'Wilfer',
-        lastname: 'Salas'
-      },
-      item: {
-        id: response?.id,
-        title: response?.title,
-        price: {
-          currency: response?.currency_id,
-          amount: response?.price,
-          decimals: response?.price
+      const product = {
+        author: {
+          name: 'Wilfer',
+          lastname: 'Salas'
         },
-        picture: response?.pictures[0]?.url,
-        condition: response?.condition,
-        free_shipping: response?.shipping?.free_shipping,
-        sold_quantity: response?.sold_quantity,
-        description: description.plain_text
+        item: {
+          id: response?.id,
+          title: response?.title,
+          price: {
+            currency: response?.currency_id,
+            amount: response?.price,
+            decimals: response?.price
+          },
+          picture: response?.pictures[0]?.url,
+          condition: response?.condition,
+          free_shipping: response?.shipping?.free_shipping,
+          sold_quantity: response?.sold_quantity,
+          description: description.plain_text
+        }
       }
-    }
 
-    return product
+      return product
+    } catch (error: any) {
+      return error.status
+    }
   }
 };
 
